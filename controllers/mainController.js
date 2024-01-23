d7bfdf050f8e9e011b0b7e87a43e6c02d11a0b49
toniquealbornoz <toniquealbornoz@yahoo.com.ar>
2024-01-23 00:27:16 -0300

    paso 2 y bonus track de la ejercitación

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,37 @@
 const path = require("path");
+
+
+const menuList=[{
+   id: 1,
+   nombre: "Carpaccio fresco",
+   descripcion: "Entrada Carpaccio de salmón con cítricos",
+   precio: "U$S 65.50",
+   imagen: "Carpaccio-de-salmon.jpg"
+},
+{
+   id: 2,
+   nombre: "Risotto de berenjena",
+   descripcion: "Risotto de berenjena y queso de cabra",
+   precio: "U$S 47.00",
+   imagen: "Risotto-berenjena-queso-cabra.jpg"
+},
+{
+   id: 3,
+   nombre: "Mousse de arroz",
+   descripcion: "Mousse de arroz con leche y aroma de azahar",
+   precio: "U$S 27.50",
+   imagen: "Mousse-de-arroz-con-leche.jpg"
+},
+{
+   id: 4,
+   nombre: "Espárragos blancos",
+   descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+   precio: "U$S 37.50",
+   imagen: "esparragos.png"
+},
+]
+
+
 const mainControllers = {
 home: (req, res )=>{
    return res.render("index.ejs",{
@@ -11,8 +44,16 @@ detalleMenu:(eq, res )=>{
    })
   },
 detalle:(req, res )=>{
-   return res.render("detalle.ejs")
+   return res.render("detalle.ejs",{
+      title:"Selecciona tu Menu",
+      menuList: menuList,
+   })
   },
+about: (req, res )=>{
+   return res.render("index.ejs",{
+      title: "Acerca de la Empresa"
+   })
+   },
 }
 
 module.exports= mainControllers;
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/", mainControllers.home)//→ c/ruta tine un controlador y usa el metodo correspondiente,ej "home", 
+router.get("/", mainControllers.home)//→ c/ruta tiene un controlador y usa el metodo correspondiente,ej "home", 
 //esta ruta responde a la raíz de nuestra app.
 
-router.get("/detalleMenu/id", mainControllers.detalleMenu)
+router.get("/detalleMenu", mainControllers.detalleMenu)
 
 router.get("/detalle/id", mainControllers.detalle)
+
+router.get("/about", mainControllers.about)
 
 module.exports = router;
